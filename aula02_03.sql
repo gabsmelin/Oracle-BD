ea8a1892f5862e602b404dc1d9a2af56366f2dcd
logonrmlocal <logonrmlocal@L1504MICRO13.fiap.com.br>
2024-02-23 21:34:39 -0300

    Aula 02 e 02, finalizadas!

--- aula02_03.sql
+++ aula02_03.sql
@@ -73,4 +73,68 @@ BEGIN
 END;
 
 
+--Ex 6
+DECLARE
+    NUMERO NUMBER(10) := &NUMERO;
+BEGIN
+    IF MOD(NUMERO, 2) = 0 THEN
+        dbms_output.put_line('O Número é Par!');
+    ELSIF MOD(NUMERO, 2) = 1 THEN
+        dbms_output.put_line('O Número é Impar!');
+    END IF;
+END;
 
+
+--Ex 7
+SET SERVEROUTPUT ON;
+DECLARE
+    NUMERO NUMBER(10) := &NUMERO;
+BEGIN
+    IF NUMERO < 0 THEN
+        dbms_output.put_line('O Número é negativo!');
+    ELSIF NUMERO >= 0 THEN
+        dbms_output.put_line('O Número é positivo!');
+    ELSE 
+        dbms_output.put_line('Algo deu errado!');
+    END IF;
+END;
+
+
+--Ex 8
+DECLARE
+    nota NUMBER(2) := '&Nota';
+BEGIN
+    IF nota >= 90 AND nota <= 100 THEN
+        dbms_output.put_line('Nota: A');
+    ELSIF nota >= 80 AND nota < 90 THEN
+        dbms_output.put_line('Nota: B!');
+     ELSIF nota >= 70 AND nota < 80 THEN
+        dbms_output.put_line('Nota: C!');
+     ELSIF nota >= 60 AND nota < 70 THEN
+        dbms_output.put_line('Nota: D!');
+    ELSIF nota >= 0 AND nota < 60 THEN
+        dbms_output.put_line('Nota: F!');
+    ELSE
+        dbms_output.put_line('Valor da nota passado de forma incorreta!');
+    END IF;
+END;
+
+
+--Ex 8
+DECLARE
+    idade NUMBER(3) := '&idade';
+BEGIN
+    IF idade >= 0 AND idade < 12 THEN
+        dbms_output.put_line('Criança');
+    ELSIF idade >= 12 AND idade < 20 THEN
+        dbms_output.put_line('Jovem!');
+     ELSIF idade >= 20 AND idade < 60 THEN
+        dbms_output.put_line('Adulto!');
+     ELSIF idade >= 60 AND idade < 90 THEN
+        dbms_output.put_line('Idoso!');
+    ELSIF idade >= 90 THEN
+        dbms_output.put_line('Ancião');
+    ELSE
+        dbms_output.put_line('Valor da idade passado de forma incorreta!');
+    END IF;
+END;
